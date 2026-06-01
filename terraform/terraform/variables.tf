f6e8793f85f71258de384620f6532354f2e7673c
Raflyanor <mardipalangkaraya@gmail.com>
2026-06-02 05:24:41 +0700

    Create variables.tf

---
+++ terraform/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "Nama resource group proyek"
+  type        = string
+  default     = "rg-booking-bengkel"
+}
+
+variable "location" {
+  description = "Lokasi resource Azure"
+  type        = string
+  default     = "East Asia"
+}
