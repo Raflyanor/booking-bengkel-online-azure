af6f6a83beae962a17cd714a78b908847eec0e73
Raflyanor <mardipalangkaraya@gmail.com>
2026-06-02 05:25:59 +0700

    Create outputs.tf

---
+++ terraform/terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "resource_group_name" {
+  description = "Resource group yang digunakan pada proyek"
+  value       = var.resource_group_name
+}
+
+output "location" {
+  description = "Region Azure yang digunakan"
+  value       = var.location
+}
