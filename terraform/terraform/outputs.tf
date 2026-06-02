bd8b2ead307407e71f32a284e20c3b3669e87a4d
Raflyanor <mardipalangkaraya@gmail.com>
2026-06-02 16:00:46 +0700

    Delete terraform/terraform/outputs.tf

--- terraform/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "resource_group_name" {
-  description = "Resource group yang digunakan pada proyek"
-  value       = var.resource_group_name
-}
-
-output "location" {
-  description = "Region Azure yang digunakan"
-  value       = var.location
-}
